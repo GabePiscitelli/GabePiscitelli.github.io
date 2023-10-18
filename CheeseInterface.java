c90a524cccb8d3f24e73c04caf53a710c0fe8584
Gabe Piscitelli <gabe.piscitelli@sapphire>
2023-10-18 08:23:14 -0400

    put in my methods to interface

---
+++ CheeseInterface.java
@@ -0,0 +1,8 @@
+public interface CheeseInterface{
+    public void add();
+    public void delete();
+    public Object userInput();
+    public Object displayImage();
+    public Object get();
+    public void insert();
+}
