10e3964f22e36ba15365bf1c0adedca71f33ca7b
Gabe Piscitelli <gabe.piscitelli@sapphire>
2023-10-25 15:11:08 -0400

    fixed search function so that it actually sends back how many time word is spelled

--- TextReader.java
+++ TextReader.java
@@ -3,7 +3,7 @@ public class TextReader implements TextReaderInterface{
 
     }
     public String takeText(){
-        String textChunk="According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.";
+        String textChunk = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.";
         return(textChunk);
     }
     public String format(String input){
@@ -20,10 +20,10 @@ public class TextReader implements TextReaderInterface{
             check[i] = word.substring(i,i+1);
         }
         for(int i=0; i<fInput.length(); i++){
-            System.out.println(check[ind]);
-            if(fInput.substring(i,i+1) == check[ind]){
+            if(fInput.substring(i,i+1).equals(check[ind])){
                 if(ind == check.length-1){
                     ret++;
+                    ind=0;
                 }
                 ind++;
             }
