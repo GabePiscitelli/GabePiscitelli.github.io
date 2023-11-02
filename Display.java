7fd7ab26747805077b20973901e5a392bf50fb00
Gabe Piscitelli <gabe.piscitelli@sapphire>
2023-11-02 08:49:34 -0400

    found way to take inputs from text box in jframe

--- Display.java
+++ Display.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 public class Display{
+    JPanel panel; // the panel is not visible in output
+    JLabel label;
+    JTextField textThing; // accepts upto 10 characters
+    JButton send;
+    JButton reset;
+    JLabel t;
+    JTextArea ta;
     public Display(){
 
     }
@@ -9,20 +16,19 @@ public class Display{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
 
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
+        panel = new JPanel(); // the panel is not visible in output
+        label = new JLabel("Enter Text");
+        textThing = new JTextField(10); // accepts upto 10 characters
+        send = new JButton("Send");
+        reset = new JButton("Reset");
         panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
+        panel.add(textThing);
         panel.add(send);
         panel.add(reset);
-
-        JButton t = new JButton();
+        t = new JLabel();
         frame.getContentPane().add(BorderLayout.NORTH, t);
         // Text Area at the Center
-        JTextArea ta = new JTextArea();
+        ta = new JTextArea();
         frame.getContentPane().add(BorderLayout.SOUTH, panel);
         frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
