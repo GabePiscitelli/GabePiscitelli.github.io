d0db2cc665c0a03f3a64373485fa7f02d06ac99c
Gabe Piscitelli <gabe.piscitelli@sapphire>
2023-12-05 09:44:00 -0500

    it works now for the most part you can put a word in and it tells you the thing

--- Display.java
+++ Display.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Display{
+    TextReader reader = new TextReader();
+    CheeseList cheeseList = new CheeseList();
     public boolean clicked = false;
     JPanel panel; // the panel is not visible in output
     JPanel bigPanel;
@@ -26,26 +28,24 @@ public class Display{
         panel = new JPanel(); // the panel is not visible in output
         bigPanel = new JPanel();
         oPanel = new JPanel();
-        oPanel.setLocation(0, 100);
+        //oPanel.setLocation(0, 100);
 
-        label = new JLabel("Enter Text");
-        textThing = new JTextField(10); // accepts upto 10 characters
         send = new JButton("Send");
         send.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                clicked = true;
+                click();
             }
         });
-        reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(textThing);
+        
+        
         panel.add(send);
-        panel.add(reset);
+        
        
-
+        label = new JLabel("");
+        panel.add(label);
         question1 = new JLabel("What would you like to search for?");
-        ans1 = new JTextField(10);
+        ans1 = new JTextField(50);
         question1.setBounds(0,0,100,50);
         ans1.setBounds(0,50,100,100);
         bigPanel.add(question1);
@@ -54,9 +54,9 @@ public class Display{
         frame.setVisible(true);
 
         question2 = new JLabel("What would you like to search for it in?");
-        ans2 = new JTextField(10);
-        question2.setBounds(0,100,100,50);
-        ans2.setBounds(0,200,100,50);
+        ans2 = new JTextField(50);
+        //question2.setBounds(0,100,100,50);
+        //ans2.setBounds(0,200,100,50);
         oPanel.add(question2);
         frame.setVisible(true);
         oPanel.add(ans2);
@@ -65,8 +65,18 @@ public class Display{
         
        
         frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(bigPanel);
+        frame.getContentPane().add(bigPanel, BorderLayout.NORTH);
         frame.getContentPane().add(BorderLayout.CENTER, oPanel);
         frame.setVisible(true);
+    }
+
+    public void click(){
+        String fInp = reader.format(ans2.getText());
+        cheeseList.userInput(ans1.getText());
+        int val = reader.search(cheeseList.value(1).data, fInp);
+        System.out.println(val);
+        String v = Integer.toString(val);
+        label.setText(v);
+        cheeseList.delete(1);
     }
 }
--- Test.java
+++ Test.java
@@ -1,9 +1,5 @@
 public class Test{
     public static void main(String args[]){
-        TextReader reader = new TextReader();
-        CheeseList cheeseList = new CheeseList();
-        Display disp = new Display();
-        
-        
+        Display disp = new Display();  
     }
 }
